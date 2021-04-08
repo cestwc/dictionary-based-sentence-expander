ca61f2a31e55a291efe547668c8e2a6a04ad2099
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-09 01:19:27 +0800

    Create grafter.py

---
+++ grafter.py
@@ -0,0 +1,58 @@
+import copy
+import random
+class ExpandedSent:
+	
+	def __init__(self, sentence, nlp):
+		self.s = sentence
+		self.nlp = nlp
+		self.doc = sentence #nlp(sentence)
+		self.rootstock = doc2root(self.doc)
+		self.scion = None
+		self.joint = None
+		self.syn = None # a specific sense in nltk.corpus wordnet Synset object
+		self.gemels = None # from the Latin word meaning "a pair"
+
+	# general (with selection on lexical categories)
+	def graft(self, lexical, tokenized_glosses, wnLemmas, conditions, wnPOS, rootPOS, common):
+		candidates = [w for w in self.rootstock.inquire(conditions[lexical]) if w.attr['lemma'] not in common]
+		if candidates == []:
+			return None
+		random.seed(42)
+		self.joint = random.choice(candidates)
+		pos = self.joint.attr['pos']
+		syn = wsd(self.rootstock.linear(), '_'.join(self.joint.attr['text'].split()), wnPOS[pos])
+		if not isSynset(syn):
+			return None
+		self.syn = syn
+		scion = doc2root(tokenized_glosses[self.syn.name()])
+		if scion.attr['pos'] not in rootPOS[pos]:
+			return None
+		self.scion = scion
+		self.rootstock = copy.deepcopy(self.rootstock)
+		if pos == 'NOUN':
+			self.gemels = self.joint.nounReplace(self.scion)
+		elif pos == 'VERB':
+			self.gemels = self.joint.verbReplace(self.scion)
+		elif pos == 'ADJ':
+			self.gemels = self.joint.adjReplace(self.scion)
+		else:           #ADV
+			self.gemels = self.joint.advReplace(self.scion)
+		self.gemels.phraseRetokenize(wnLemmas)
+		return None
+
+
+	def show(self):
+		if self.gemels == None:
+			return 'N/A'
+		print(*[self.syn.definition(), self.rootstock.sent(), self.gemels.sent()], sep = '\n')
+		displacy.render(self.doc, style='dep', jupyter = True,  options={'distance': 100})
+		displacy.render(self.contrast, style='dep', jupyter = True,  options={'distance': 100})
+
+	def evaluate(self):
+		if self.gemels == None:
+			return None, None, None
+		self.contrast = phraseTokenize(self.nlp(self.gemels.sent()))
+		contrastLinear = doc2linear(self.contrast)
+		gemelLinear = self.gemels.linear()
+		difference = [(gemelLinear[i], contrastLinear[i]) for i in range(min(len(gemelLinear), len(contrastLinear))) if not gemelLinear[i].lookLike(contrastLinear[i])]
+		return (len(difference), 1 - len(difference) / len(gemelLinear), difference)# if difference != None else (None, None, None)
