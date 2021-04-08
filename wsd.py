8f773159f8fbcfef872eb991d926c3198272135b
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-09 01:17:57 +0800

    Update wsd.py

--- wsd.py
+++ wsd.py
@@ -1,19 +1,20 @@
 #synset nltk
 def isSynset(syn):
-    try:
-        if syn == None:
-            return 0
-    except:
-        return 1
+	
+	try:
+		if syn == None:
+			return 0
+	except:
+		return 1
 
 def wsd(tokens, word, pos = None):
-    #lesk nltk
-    if pos == None:
-        return lesk(tokens, word)
-    else:
-        i = 0
-        syn = None
-        while i < len(pos) and not isSynset(syn):
-            syn = lesk(tokens, word, pos[i])
-            i += 1
-        return syn
+	#lesk nltk
+	if pos == None:
+		return lesk(tokens, word)
+	else:
+		i = 0
+		syn = None
+		while i < len(pos) and not isSynset(syn):
+			syn = lesk(tokens, word, pos[i])
+			i += 1
+		return syn
