481f503829e08e0db726a98d5527097269883708
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-09 22:21:10 +0800

    Update replicator.py

--- replicator.py
+++ replicator.py
@@ -1,6 +1,8 @@
 import spacy
 spacy.prefer_gpu()
 
+from node import Node
+
 def token2node(t, cols, headNode = None):
 	
 	rows = [t.text, t.lemma_, t.pos_, t.tag_, spacy.explain(t.pos_), t.dep_, t.morph.to_dict(), t.is_stop, t.n_lefts, t.n_rights]
