d90eff68d560a9e5878da4ccce811ce52d219122
cestwc <80936226+cestwc@users.noreply.github.com>
2021-04-09 16:24:13 +0800

    Update and rename utils.py to util_common.py

---
+++ util_common.py
@@ -0,0 +1,14 @@
+# to obtain a list of common words in English
+# we may have multiple ways to do that, here we use a hardcoded word list 'common.csv'
+
+import csv
+from itertools import chain
+
+def commonWords():
+	
+	commonWords = {}
+	with open('common.csv','r') as f: 
+		for line in csv.reader(f): 
+			commonWords[line[0]] = [w for w in line[1:] if w != '']
+			
+	return list(chain(*[v for v in commonWords().values()]))
